f5163d612b0d8a109991d21170a7b7f9ea786514
7LPdWcaW <7LPdWcaW@microgrowery>
2017-02-17 21:48:36 +0000

    Added last check functionaity

--- app/src/main/java/me/anon/grow/updater/ConfigureActivity.java
+++ app/src/main/java/me/anon/grow/updater/ConfigureActivity.java
@@ -1,6 +1,7 @@
 package me.anon.grow.updater;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -11,6 +12,10 @@ import android.text.Html;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * // TODO: Add class description
@@ -32,7 +37,7 @@ public class ConfigureActivity extends AppCompatActivity
 		}
 	}
 
-	public static class ConfigureFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener
+	public static class ConfigureFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener, CheckUpdateReceiver.CheckCallbackListener
 	{
 		private static final int REQUEST_STORAGE_PERMISSION = 0x1;
 
@@ -43,6 +48,23 @@ public class ConfigureActivity extends AppCompatActivity
 			addPreferencesFromResource(R.xml.preferences);
 
 			findPreference("about").setOnPreferenceClickListener(this);
+			findPreference("last_checked").setOnPreferenceClickListener(this);
+
+			long lastChecked = getPreferenceManager().getSharedPreferences().getLong("last_checked", 0);
+			String lastCheckedDateStr = "never";
+
+			if (lastChecked > 0)
+			{
+				DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getActivity());
+				DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(getActivity());
+
+				Date actionDate = new Date(lastChecked);
+				Calendar actionCalendar = GregorianCalendar.getInstance();
+				actionCalendar.setTime(actionDate);
+				lastCheckedDateStr = dateFormat.format(actionDate) + " " + timeFormat.format(actionDate);
+			}
+
+			findPreference("last_checked").setTitle("Last checked: " + lastCheckedDateStr);
 		}
 
 		@Override public boolean onPreferenceClick(Preference preference)
@@ -74,8 +96,25 @@ public class ConfigureActivity extends AppCompatActivity
 
 				return true;
 			}
+			else if (preference.getKey().equals("last_checked"))
+			{
+				CheckUpdateReceiver.checkCallback = this;
+				Intent broadcast = new Intent(getActivity(), CheckUpdateReceiver.class);
+				getActivity().sendBroadcast(broadcast);
+			}
 
 			return false;
+		}
+
+		@Override public void onDestroy()
+		{
+			CheckUpdateReceiver.checkCallback = null;
+			super.onDestroy();
+		}
+
+		@Override public void onUpdateChecked(boolean downloaded)
+		{
+			onCreate(null);
 		}
 	}
 }
